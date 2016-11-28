780d59624ac7d0dfdde7ea1f02112665b78d157c
Gresa Shala <gresas@stud.ntnu.no>
2016-11-29 00:09:35 +0100

    Added classes for local database management and communication to the server

---
+++ app/src/main/java/com/example/florim/thirremjeshtrin/ServerConnect.java
@@ -0,0 +1,144 @@
+package com.example.florim.thirremjeshtrin;
+
+import android.os.AsyncTask;
+import android.util.JsonReader;
+
+
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Gresa on 28-Nov-16.
+ */
+
+/**
+ * Class that provides the interface needed to create a connection to the network via HTTP, send requests
+ * and get the data returned as a response
+ */
+public class ConnectToServer {
+    /**
+     * The url to initiate the HTTP connection to
+     */
+    public String url;
+    /**
+     * The parameters to be sent with the HTTP request by POST method
+     */
+    private String urlParameters;
+    /**
+     * The data returned from the HTTP responsed, parsed from JSON
+     */
+    public  List<Map<String,String>> results;
+
+    /**
+     * The constructor for the class
+     * @param url the url that the object will communicate via HTTP with
+     * @param urlParameters the parameters that will be sent with the HTTP request
+     */
+  public ConnectToServer(String url,String urlParameters){
+      this.url=url;
+      this.urlParameters=urlParameters;
+      new NetworkTask().execute(this);
+
+  }
+
+    /**
+     * A subclass of Asynctask that makes network communication possible in the background
+      */
+    private class NetworkTask extends AsyncTask<ConnectToServer, Void, Void> {
+
+        @Override
+        protected Void doInBackground(ConnectToServer... requestContents) {
+
+
+            for (ConnectToServer request : requestContents) {
+
+                 getResponse(request);
+            }
+           return null;
+
+        }
+
+        /**
+         * Method that creates a URL Connection via HTTP, sends a request by POST method and saves the response as a result variable of the
+         * ConnectToServer object
+         * @param request The ConnectToServer object that is used to get the URL, the URL Parameters and then save the response data
+         */
+        private void getResponse(ConnectToServer request) {
+            JsonReader jReader;
+            try {
+                URL url = new URL(request.url);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                urlConnection.setDoOutput(true);
+                urlConnection.setDoInput(true);
+                urlConnection.setRequestMethod("POST");
+
+                byte[] postData       = request.urlParameters.getBytes(StandardCharsets.UTF_8);
+                int    postDataLength = postData.length;
+
+                urlConnection.connect();
+
+                urlConnection.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+                urlConnection.setRequestProperty( "charset", "utf-8");
+                urlConnection.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+                urlConnection.setUseCaches( false );
+                urlConnection.getOutputStream().write( postData );
+
+
+                InputStream is = urlConnection.getInputStream();
+                jReader = new JsonReader(new InputStreamReader(is, "UTF-8"));
+                urlConnection.disconnect();
+
+                request.results=ParseJson(jReader);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+
+        }
+
+        /**
+         * Method that parses a JSON Array into a Map List
+         * @param jsonReader The object that contains unparsed JSON data
+         * @return The JSON data, parsed
+         */
+        private List<Map<String,String>> ParseJson(JsonReader jsonReader){
+           try {
+               List<Map<String, String>> results = new ArrayList<>();
+               Map<String, String> row;
+               if (jsonReader != null) {
+                   jsonReader.beginArray();
+                   while (jsonReader.hasNext()) {
+                       
+                        jsonReader.beginObject();                   
+                       row = new HashMap<String, String>();
+                       while (jsonReader.hasNext()) {
+                           row.put(jsonReader.nextName(), jsonReader.nextString());
+                       }
+                            results.add(row);
+                           jsonReader.endObject();
+
+                       }
+                   jsonReader.endArray();
+                   }
+               return results;
+               }
+           
+           catch(IOException ex){
+               ex.printStackTrace();
+               return null;
+           }
+    }
+
+}
---
+++ app/src/main/java/com/example/florim/thirremjeshtrin/ThirreMjeshtrinDb.java
@@ -0,0 +1,207 @@
+package com.example.florim.thirremjeshtrin;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+import android.util.JsonReader;
+
+/**
+ * Created by Gresa on 28-Nov-16.
+ */
+
+/**
+ * A class that provides the needed infrastructure to the app to communicate with the local database, in order to get information from it,
+ * or feed it information
+ */
+public class ThirreMjeshtrinDb extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    private static final int DATABASE_VERSION =1;
+    private static final String DATABASE_NAME = "ThirreMjeshtrin.db";
+    private SQLiteDatabase db = this.getWritableDatabase();
+    public ThirreMjeshtrinDb(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * A static class that has it's own _ID variable to help keep track of records in a database
+     * table and also other final string variables for easier SQL query writing for the table
+     * Inbox- caches data on the user's conversations with other users
+     */
+    static class Inbox implements BaseColumns {
+        public static final String TABLE_NAME = "Inbox";
+        public static final String COLUMN_USERNAME = "Username";
+        public static final String COLUMN_LASTMESSAGE = "LastMessage";
+        public static final String COLUMN_TIMESTAMP = "Timestamp";
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ( "
+                        + _ID +
+                        " INTEGER PRIMARY KEY , " + COLUMN_USERNAME + " VARCHAR(30) ," +
+                        COLUMN_LASTMESSAGE + " TEXT," + COLUMN_TIMESTAMP + " TIMESTAMP)";
+
+
+        private static final String SQL_TRUNCATE_ENTRIES =
+                "TRUNCATE TABLE IF EXISTS " + TABLE_NAME;
+
+        private static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    }
+    /**
+     * A static class that has it's own _ID variable to help keep track of records in a database
+     * table and also other final string variables for easier SQL query writing for the table
+     * Profile- caches the profile data for an ordinary user
+     */
+    static class Profile implements BaseColumns {
+        public static final String TABLE_NAME = "Profile";
+        public static final String COLUMN_USERNAME = "Username";
+        public static final String COLUMN_EMAIL = "Email";
+        public static final String COLUMN_USERID = "UserID";
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ( "
+                        + _ID +
+                        " INTEGER PRIMARY KEY , " + COLUMN_USERNAME + " VARCHAR(30) ," +
+                        COLUMN_EMAIL + " VARCHAR(50)," + COLUMN_USERID + " INT(11)) ";
+
+
+        private static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+        private static final String SQL_TRUNCATE_ENTRIES =
+                "TRUNCATE TABLE IF EXISTS " + TABLE_NAME;
+
+    }
+    /**
+     * A static class that has it's own _ID variable to help keep track of records in a database
+     * table and also other final string variables for easier SQL query writing for the table
+     * ReapairmanProfile- caches profile data for a repairman user
+     */
+    static class RepairmanProfile implements BaseColumns {
+        public static final String TABLE_NAME = "RepairmanProfile";
+        public static final String COLUMN_USERNAME = "Username";
+        public static final String COLUMN_EMAIL = "Email";
+        public static final String COLUMN_NAME="Name";
+        public static final String COLUMN_SURNAME="Surname";
+        public static final String COLUMN_LOCATION="Location";
+        public static final String COLUMN_RADIUS = "Radius";
+        public static final String COLUMN_CATEGORY = "Category";
+        public static final String COLUMN_PHONE = "Phone";
+        public static final String COLUMN_RATING = "Rating";
+        public static final String COLUMN_USERID = "UserID";
+
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ( "
+                        + _ID +
+                        " INTEGER PRIMARY KEY , " + COLUMN_USERNAME + " VARCHAR(30) ," +
+                        COLUMN_EMAIL + " VARCHAR(50)," + COLUMN_NAME + " VARCHAR(70) ,"+
+                        COLUMN_SURNAME + " VARCHAR(70) ,"+ COLUMN_LOCATION + " TEXT ,"+
+                        COLUMN_RADIUS + " INTEGER ,"+ COLUMN_CATEGORY + " VARCHAR(30),"+ COLUMN_PHONE + " VARCHAR(30) ,"+
+                        COLUMN_RATING + " VARCHAR(30) ,"+ COLUMN_USERID + " INT(11)) ";
+
+
+        private static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+        private static final String SQL_TRUNCATE_ENTRIES =
+                "TRUNCATE TABLE IF EXISTS " + TABLE_NAME;
+
+    }
+
+    /**
+     * Truncates the existing Inbox table in the database and feeds it new data from the ContentValues parameter
+     * @param values contains the data that will be inserted in the Inbox table
+     */
+    public void updateInbox(ContentValues values){
+        db.execSQL(Inbox.SQL_TRUNCATE_ENTRIES);
+        db.insert(Inbox.TABLE_NAME,null,values);
+    }
+
+    /**
+     * Reads all the data in the Inbox table, ordered from newest to oldest
+     * @return Cursor containing the data returned from query execution on the table
+     */
+    public Cursor getInbox(){
+
+        String query= "SELECT * FROM "+Inbox.TABLE_NAME+" ORDER BY "+Inbox._ID+ " DESC";
+        Cursor c=db.rawQuery(query,null,null);
+        return c;
+    }
+
+    /**
+     * Truncates the existing RepairmanProfile or Profile table in the database and feeds it new data from the ContentValues parameter
+     * @param values contains the data that will be inserted in the RepairmanProfile or Profile table
+     * @param isRepairman works as a switch deciding which one of RepairmanProfile or Profile table will be updated
+     */
+    public void updateProfile(ContentValues values,boolean isRepairman){
+        if(isRepairman){
+            db.execSQL(RepairmanProfile.SQL_TRUNCATE_ENTRIES);
+            db.insert(RepairmanProfile.TABLE_NAME,null,values);
+        }
+        else{
+            db.execSQL(Profile.SQL_TRUNCATE_ENTRIES);
+            db.insert(Profile.TABLE_NAME,null,values);
+        }
+    }
+
+    /**
+     * Reads all the data in the RepairmanProfile or Profile table
+     * @param isRepairman works as a switch deciding which one of RepairmanProfile or Profile table will be read
+     * @return profileData, a ContentValues object, with the data read from the chosen table
+     */
+    public ContentValues getProfile(boolean isRepairman){
+        ContentValues profileData=new ContentValues();
+        if(isRepairman){
+            String query= "SELECT * FROM "+RepairmanProfile.TABLE_NAME;
+            Cursor c=db.rawQuery(query,null,null);
+            c.moveToFirst();
+            profileData.put(RepairmanProfile.COLUMN_USERNAME,c.getString(c.getColumnIndex(RepairmanProfile.COLUMN_USERNAME)));
+            profileData.put(RepairmanProfile.COLUMN_EMAIL,c.getString(c.getColumnIndex(RepairmanProfile.COLUMN_EMAIL)));
+            profileData.put(RepairmanProfile.COLUMN_NAME,c.getString(c.getColumnIndex(RepairmanProfile.COLUMN_NAME)));
+            profileData.put(RepairmanProfile.COLUMN_SURNAME,c.getString(c.getColumnIndex(RepairmanProfile.COLUMN_SURNAME)));
+            profileData.put(RepairmanProfile.COLUMN_LOCATION,c.getString(c.getColumnIndex(RepairmanProfile.COLUMN_LOCATION)));
+            profileData.put(RepairmanProfile.COLUMN_RADIUS,c.getString(c.getColumnIndex(RepairmanProfile.COLUMN_RADIUS)));
+            profileData.put(RepairmanProfile.COLUMN_CATEGORY,c.getString(c.getColumnIndex(RepairmanProfile.COLUMN_CATEGORY)));
+            profileData.put(RepairmanProfile.COLUMN_PHONE,c.getString(c.getColumnIndex(RepairmanProfile.COLUMN_PHONE)));
+            profileData.put(RepairmanProfile.COLUMN_RATING,c.getString(c.getColumnIndex(RepairmanProfile.COLUMN_RATING)));
+            profileData.put(RepairmanProfile.COLUMN_USERID,c.getString(c.getColumnIndex(RepairmanProfile.COLUMN_USERID)));
+            return profileData;
+        }
+
+        String query= "SELECT * FROM "+Profile.TABLE_NAME;
+        Cursor c=db.rawQuery(query,null,null);
+        c.moveToFirst();
+        profileData.put(Profile.COLUMN_USERNAME,c.getString(c.getColumnIndex(Profile.COLUMN_USERNAME)));
+        profileData.put(Profile.COLUMN_EMAIL,c.getString(c.getColumnIndex(Profile.COLUMN_EMAIL)));
+        profileData.put(Profile.COLUMN_USERID,c.getString(c.getColumnIndex(Profile.COLUMN_USERID)));
+        return profileData;
+
+    }
+
+
+    /**
+     * Method called on the creation of a ThirreMjeshtrinDb object
+     * @param db
+     */
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Inbox.SQL_CREATE_TABLE);
+        db.execSQL(Inbox.SQL_CREATE_TABLE);
+        db.execSQL(Inbox.SQL_CREATE_TABLE);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(Inbox.SQL_DELETE_ENTRIES);
+        db.execSQL(Profile.SQL_DELETE_ENTRIES);
+        db.execSQL(RepairmanProfile.SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+
+    }
+
+}
