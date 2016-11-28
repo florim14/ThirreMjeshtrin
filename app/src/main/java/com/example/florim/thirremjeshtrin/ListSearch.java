eaa789c3cc032f488ffdca8c1fcb919f05c709a1
vigan <viganm@stud.ntnu.no>
2016-11-28 19:59:31 +0100

    Fragment manipulation

--- app/src/main/java/com/example/florim/thirremjeshtrin/ListSearch.java
+++ app/src/main/java/com/example/florim/thirremjeshtrin/ListSearch.java
@@ -1,39 +1,71 @@
 package com.example.florim.thirremjeshtrin;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class ListSearch extends AppCompatActivity {
+public class ListSearch extends Fragment{
 
     ArrayList<String> listItems;
     ArrayAdapter<String> adapter;
     String[] array;
     ListView listView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_search);
+    OnItemClickListener onItemClickListener;
 
-        listView = (ListView)findViewById(R.id.lstView);
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_list_search, container, false);
+        listView = (ListView)view.findViewById(R.id.lstView);
         array = getResources().getStringArray(R.array.array_country);
-        listItems.addAll(Arrays.asList(array));
-        adapter = new ArrayAdapter<String>(ListSearch.this, android.R.layout.simple_list_item_1, listItems);
+        //listItems.addAll(Arrays.asList(array));
+        adapter = new ArrayAdapter<String>(getActivity(), R.layout.headline_list_layout, R.id.row_item, array);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = array[i];
+                onItemClickListener.itemSelected(item);
+            }
+        });
+        return view;
+    }
+
+    public interface OnItemClickListener{
+        public void itemSelected(String item);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            onItemClickListener = (OnItemClickListener) context;
+        }catch (Exception e){}
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_search, menu);
         MenuItem item = menu.findItem(R.id.menuSearch);
         SearchView searchView = (SearchView)item.getActionView();
@@ -49,6 +81,5 @@ public class ListSearch extends AppCompatActivity {
                 return false;
             }
         });
-        return super.onCreateOptionsMenu(menu);
     }
 }
