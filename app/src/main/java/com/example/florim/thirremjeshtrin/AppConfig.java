74a63bd2ea2686d53485d403743c74ea7ec3aa66
vigan <viganm@stud.ntnu.no>
2016-11-29 00:29:12 +0100

    Login, Register and Welcome stuff done

---
+++ app/src/main/java/com/example/florim/thirremjeshtrin/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.florim.thirremjeshtrin;
+
+/**
+ * Created by vigan on 11/29/2016.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.102/android_login_api/register.php";
+}
