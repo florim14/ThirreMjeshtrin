c5070a6ccd1b23dac39e66c0b12685c26b9286c4
Gresa Shala <gresas@stud.ntnu.no>
2016-11-29 00:12:07 +0100

    Added classes for local database management and communication to the server

--- app/src/main/java/com/example/florim/thirremjeshtrin/ConnectToServer.java
+++ app/src/main/java/com/example/florim/thirremjeshtrin/ConnectToServer.java
@@ -37,23 +37,24 @@ public class ConnectToServer {
     /**
      * The data returned from the HTTP responsed, parsed from JSON
      */
-    public  List<Map<String,String>> results;
+    public List<Map<String, String>> results;
 
     /**
      * The constructor for the class
-     * @param url the url that the object will communicate via HTTP with
+     *
+     * @param url           the url that the object will communicate via HTTP with
      * @param urlParameters the parameters that will be sent with the HTTP request
      */
-  public ConnectToServer(String url,String urlParameters){
-      this.url=url;
-      this.urlParameters=urlParameters;
-      new NetworkTask().execute(this);
+    public ConnectToServer(String url, String urlParameters) {
+        this.url = url;
+        this.urlParameters = urlParameters;
+        new NetworkTask().execute(this);
 
-  }
+    }
 
     /**
      * A subclass of Asynctask that makes network communication possible in the background
-      */
+     */
     private class NetworkTask extends AsyncTask<ConnectToServer, Void, Void> {
 
         @Override
@@ -62,15 +63,16 @@ public class ConnectToServer {
 
             for (ConnectToServer request : requestContents) {
 
-                 getResponse(request);
+                getResponse(request);
             }
-           return null;
+            return null;
 
         }
 
         /**
          * Method that creates a URL Connection via HTTP, sends a request by POST method and saves the response as a result variable of the
          * ConnectToServer object
+         *
          * @param request The ConnectToServer object that is used to get the URL, the URL Parameters and then save the response data
          */
         private void getResponse(ConnectToServer request) {
@@ -83,62 +85,62 @@ public class ConnectToServer {
                 urlConnection.setDoInput(true);
                 urlConnection.setRequestMethod("POST");
 
-                byte[] postData       = request.urlParameters.getBytes(StandardCharsets.UTF_8);
-                int    postDataLength = postData.length;
+                byte[] postData = request.urlParameters.getBytes(StandardCharsets.UTF_8);
+                int postDataLength = postData.length;
 
                 urlConnection.connect();
 
-                urlConnection.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
-                urlConnection.setRequestProperty( "charset", "utf-8");
-                urlConnection.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
-                urlConnection.setUseCaches( false );
-                urlConnection.getOutputStream().write( postData );
+                urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                urlConnection.setRequestProperty("charset", "utf-8");
+                urlConnection.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+                urlConnection.setUseCaches(false);
+                urlConnection.getOutputStream().write(postData);
 
 
                 InputStream is = urlConnection.getInputStream();
                 jReader = new JsonReader(new InputStreamReader(is, "UTF-8"));
                 urlConnection.disconnect();
 
-                request.results=ParseJson(jReader);
+                request.results = ParseJson(jReader);
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            
+
 
         }
 
         /**
          * Method that parses a JSON Array into a Map List
+         *
          * @param jsonReader The object that contains unparsed JSON data
          * @return The JSON data, parsed
          */
-        private List<Map<String,String>> ParseJson(JsonReader jsonReader){
-           try {
-               List<Map<String, String>> results = new ArrayList<>();
-               Map<String, String> row;
-               if (jsonReader != null) {
-                   jsonReader.beginArray();
-                   while (jsonReader.hasNext()) {
-                       
-                        jsonReader.beginObject();                   
-                       row = new HashMap<String, String>();
-                       while (jsonReader.hasNext()) {
-                           row.put(jsonReader.nextName(), jsonReader.nextString());
-                       }
-                            results.add(row);
-                           jsonReader.endObject();
+        private List<Map<String, String>> ParseJson(JsonReader jsonReader) {
+            try {
+                List<Map<String, String>> results = new ArrayList<>();
+                Map<String, String> row;
+                if (jsonReader != null) {
+                    jsonReader.beginArray();
+                    while (jsonReader.hasNext()) {
 
-                       }
-                   jsonReader.endArray();
-                   }
-               return results;
-               }
-           
-           catch(IOException ex){
-               ex.printStackTrace();
-               return null;
-           }
+                        jsonReader.beginObject();
+                        row = new HashMap<String, String>();
+                        while (jsonReader.hasNext()) {
+                            row.put(jsonReader.nextName(), jsonReader.nextString());
+                        }
+                        results.add(row);
+                        jsonReader.endObject();
+
+                    }
+                    jsonReader.endArray();
+                }
+                return results;
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                return null;
+            }
+        }
+
     }
-
 }
