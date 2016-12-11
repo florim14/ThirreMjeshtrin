c5dba4964318c0ab2e6eef4e409176e3d46a7dfd
Gresa Shala <gresas@stud.ntnu.no>
2016-12-11 16:55:54 +0100

    Notification management

--- app/src/main/java/com/example/florim/thirremjeshtrin/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/florim/thirremjeshtrin/MyFirebaseMessagingService.java
@@ -30,36 +30,59 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         sendNotification(remoteMessage);
 
     }
-    private void sendNotification(RemoteMessage remoteMessage){
-        String type=remoteMessage.getData().get("type");
-        String from=remoteMessage.getData().get("From");
-        String ID=remoteMessage.getData().get("ID");
+    private void sendNotification(RemoteMessage remoteMessage) {
+        String type = remoteMessage.getData().get("type");
+        String from = remoteMessage.getData().get("From");
+        String ID = remoteMessage.getData().get("ID");
         Intent intent;
-        PendingIntent pIntent=null;
-        if(type!=null) {
+        PendingIntent pIntent = null;
+        if (type != null) {
             if (type.equals("request")) {
                 intent = new Intent(this, Request.class);
                 intent.putExtra("from", from);
                 intent.putExtra("ID", ID);
-                intent.putExtra("Message", remoteMessage.getNotification().getBody() + ID);
+                intent.putExtra("Message", remoteMessage.getNotification().getBody());
                 pIntent = PendingIntent.getActivity(this, Integer.valueOf(ID), intent, PendingIntent.FLAG_CANCEL_CURRENT);
-            }
-            else if(type.equals("message")) {
+            } else if (type.equals("message")) {
                 newMessageToActivity();
+            } else if (type.equals("answer")) {
+
+                String status = remoteMessage.getData().get("status");
+                if (status.equals("ACCEPTED")) {
+                    intent = new Intent(this, FeedbackTab.class);
+                    String Username = remoteMessage.getData().get("Username");
+                    String Email = remoteMessage.getData().get("Email");
+                    String UserID = remoteMessage.getData().get("ID");
+                    String Phone = remoteMessage.getData().get("Phone");
+                    String Lat = remoteMessage.getData().get("Lat");
+                    String Lon = remoteMessage.getData().get("Lon");
+                    String Radius = remoteMessage.getData().get("Radius");
+                    String Category = remoteMessage.getData().get("Category");
+
+                    intent.putExtra("RepairmanID", UserID);
+                    intent.putExtra("Username", Username);
+                    intent.putExtra("Lat", Lat);
+                    intent.putExtra("Lon", Lon);
+                    intent.putExtra("Radius", Radius);
+                    intent.putExtra("Phone", Phone);
+                    intent.putExtra("Email", Email);
+                    intent.putExtra("Category", Category);
+                    pIntent = PendingIntent.getActivity(this, Integer.valueOf(ID), intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                }
             }
+            // build notification
+            // the addAction re-use the same intent to keep the example short
+            Notification n = new Notification.Builder(this)
+                    .setContentTitle(remoteMessage.getNotification().getTitle())
+                    .setContentText(remoteMessage.getNotification().getBody())
+                    .setSmallIcon(R.drawable.ic_wrench_png_19773)
+                    .setContentIntent(pIntent)
+                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                    .setAutoCancel(true)
+                    .build();
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(0, n);
         }
-        // build notification
-        // the addAction re-use the same intent to keep the example short
-        Notification n = new Notification.Builder(this)
-                .setContentTitle(remoteMessage.getNotification().getTitle())
-                .setContentText(remoteMessage.getNotification().getBody() + ID)
-                .setSmallIcon(R.drawable.ic_wrench_png_19773)
-                .setContentIntent(pIntent)
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setAutoCancel(true)
-                .build();
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(0, n);
     }
 
     private void newMessageToActivity() {
